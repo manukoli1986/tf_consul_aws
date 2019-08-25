e392a716e7ed2a266e90a330e23c8fc2292577d0
mayank <manukoli1986@gmail.com>
2019-08-25 05:32:21 -0700

    updated file

--- consul_aws/output.tf
+++ consul_aws/output.tf
@@ -1,3 +1,11 @@
-output "ec2_global_ips" {
-  value = ["${aws_instance.consul_server_1.*.public_ip}"]
+output "ips1" {
+  value = ["${join(",", aws_instance.consul_server_1.*.public_ip)}"]
+}
+
+output "ips2" {
+  value = ["${join(",", aws_instance.consul_server_2.*.public_ip)}"]
+}
+
+output "ips3" {
+  value = ["${join(",", aws_instance.consul_server_3.*.public_ip)}"]
 }
